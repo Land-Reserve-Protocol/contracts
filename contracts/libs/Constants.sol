e5a9b3ec66d29f87d5a62f660eca26f1235832ba
Kingsley Victor <javaprodigy56@gmail.com>
2025-08-10 16:16:29 +0100

    chore: some kind updates sha

--- contracts/libs/Constants.sol
+++ contracts/libs/Constants.sol
@@ -10,3 +10,8 @@ bytes32 constant REGISTRY_UPDATER_ROLE = keccak256('REGISTRY_UPDATER');
 bytes32 constant METADATA_UPDATER_ROLE_PREFIX = keccak256('METADATA_UPDATER_PREFIX');
 // Math
 uint24 constant BASE_NON_NATIVE_UNIT = 10000;
+// Category multiplier deltas
+uint24 constant RESIDENTIAL = 0; // 0.0
+uint24 constant COMMERCIAL = 4000; // 0.4
+uint24 constant INDUSTRIAL = 6000; //0.6 (until changes are requested)
+uint24 constant AGRICULTURAL = 1000; // 0.1
