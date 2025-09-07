8b4a93eeb2d288756f4e6136778f22f6349a2e2b
Kingsley Victor <javaprodigy56@gmail.com>
2025-09-07 23:37:13 +0100

    chore: updates

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,7 +1,19 @@
 import { id } from 'ethers';
 
+type VariablesType = {
+  team: string;
+  councilMembers: string[];
+};
+
 export const MINTER_ROLE = id('MINTER');
 export const COUNCIL_MEMBER_ROLE = id('COUNCIL_MEMBER');
 export const RELAYER_ROLE = id('RELAYER');
 export const ROLES_GOVERNOR_ROLE = id('ROLES_GOVERNOR');
 export const REGISTRY_UPDATER_ROLE = id('REGISTRY_UPDATER');
+
+export const VARIABLES: { [key: number]: VariablesType } = {
+  20119: {
+    team: '0xb69DB7b7B3aD64d53126DCD1f4D5fBDaea4fF578',
+    councilMembers: ['0xb69DB7b7B3aD64d53126DCD1f4D5fBDaea4fF578'],
+  },
+};
